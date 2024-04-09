966492bd8b773538b3a7c71083efc7d0964dd4b4
Alejandra zapata castaneda <ale-0324@hotmail.com>
2024-04-08 22:56:45 -0500

    Se implementaron los atributos y metodos de acceso de la clase administrador ademas de su constructor.

--- src/autonoma/hospital/models/Administrador.java
+++ src/autonoma/hospital/models/Administrador.java
@@ -7,5 +7,63 @@ package autonoma.hospital.models;
  */
 public class Administrador
 {
+  ////////////Atributos
     
+   private String nombre;
+   private String numeroIdentificacion;
+   private Integer edad;
+   private String carrera;
+   
+   //////////////Constructor/////////////////////////
+
+    public Administrador(String nombre, String numeroIdentificacion, Integer edad, String carrera)
+    {
+        this.nombre = nombre;
+        this.numeroIdentificacion = numeroIdentificacion;
+        this.edad = edad;
+        this.carrera = carrera;
+    }
+   
+    //////////////Metodos De Acceso///////////////////////////////////////
+
+    public String getNombre()
+    {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) 
+    {
+        this.nombre = nombre;
+    }
+
+    public String getNumeroIdentificacion()
+    {
+        return numeroIdentificacion;
+    }
+
+    public void setNumeroIdentificacion(String numeroIdentificacion)
+    {
+        this.numeroIdentificacion = numeroIdentificacion;
+    }
+
+    public Integer getEdad()
+    {
+        return edad;
+    }
+
+    public void setEdad(Integer edad)
+    {
+        this.edad = edad;
+    }
+
+    public String getCarrera()
+    {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) 
+    {
+        this.carrera = carrera;
+    }
+   
 }
