6de84a0d0fa0c5c2ca9d8167ed3d0849f5a69907
Alejandra zapata castaneda <ale-0324@hotmail.com>
2024-04-09 21:57:32 -0500

    Corrigiendo errores.

--- src/autonoma/hospital/models/Padecimiento.java
+++ src/autonoma/hospital/models/Padecimiento.java
@@ -7,5 +7,50 @@ package autonoma.hospital.models;
  */
 public class Padecimiento
 {
+    /////////////////////////Atributos///////////////////
+   /**
+    * Nombre de la enfermedad 
+    */
+   private String nombre;
+   /**
+    * Descripcion de enfermedad 
+    */
+   private String descripcion;
+   
+   ////////////////////Constructor///////////////////////
+    /**
+     * Inicializa valores de las variables 
+     * @param nombre de la enfermedad
+     * @param descripcion de la enfermedad 
+     */
+    public Padecimiento(String nombre, String descripcion)
+    {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+   
+    ///////////////////Metodos De Acceso///////////////////
+
+    public String getNombre() 
+    {
+        return nombre;
+    }
+
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() 
+    {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion)
+    {
+        this.descripcion = descripcion;
+    }
     
+    
+   
 }
