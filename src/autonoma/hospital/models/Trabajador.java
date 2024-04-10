c18341ca93eb7b339ad4c1de83b1d180e5b5e3b7
Alejandra zapata castaneda <ale-0324@hotmail.com>
2024-04-09 21:39:09 -0500

    se implementaron los metodos de acceso de la clase Hospital y se crearon los atributos,constructor y metodos de acceso de la clase Trabajador.

--- src/autonoma/hospital/models/Trabajador.java
+++ src/autonoma/hospital/models/Trabajador.java
@@ -7,5 +7,73 @@ package autonoma.hospital.models;
  */
 public class Trabajador
 {
+    /**
+     * Nombre del trabajador
+     */
+    private String nombre;
+    /**
+     * Id del trabajador 
+     */
+    private String numeroDeIdentificacion;
+    /**
+     * Edad del trabajador
+     */
+    private String edad;
+    /**
+     * Salario base del trabajador
+     */
+    private String salarioBase;
+    
+    //////////////Constructor////////////////////////
+    
+    /**
+     * Inicializa valores de las variables 
+     * @param nombre del trabajador
+     * @param numeroDeIdentificacion del trabajador
+     * @param edad del trabajador
+     * @param salarioBase  del trabajador
+     */
+   
+    public Trabajador(String nombre, String numeroDeIdentificacion, String edad, String salarioBase) {
+        this.nombre = nombre;
+        this.numeroDeIdentificacion = numeroDeIdentificacion;
+        this.edad = edad;
+        this.salarioBase = salarioBase;
+    }
+    
+   ///////////////Metodos De Acceso/////////////////////////// 
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNumeroDeIdentificacion() {
+        return numeroDeIdentificacion;
+    }
+
+    public void setNumeroDeIdentificacion(String numeroDeIdentificacion) {
+        this.numeroDeIdentificacion = numeroDeIdentificacion;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(String salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+    
     
 }
