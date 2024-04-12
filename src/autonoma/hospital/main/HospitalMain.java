5e1adc5266ad242a002eb1ce95e69334ee3c0205
Samuel Esteban Herrera Bedoya <samuel.herrera.bedoya@gmail.com>
2024-04-11 19:00:45 -0500

    Pruebas en el manejo de archivos desde la clase GeneradorReportes.

--- src/autonoma/hospital/main/HospitalMain.java
+++ src/autonoma/hospital/main/HospitalMain.java
@@ -1,5 +1,8 @@
 package autonoma.hospital.main;
 
+import javax.swing.JOptionPane;
+import autonoma.hospital.models.GeneradorReportes;
+import static autonoma.hospital.models.GeneradorReportes.escribirEnArchivo;
 /**
  *
  * @author Samuel Esteban Herrera Bedoya & Alejandra Zapata Castañeda
@@ -9,8 +12,15 @@ public class HospitalMain
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
-        // TODO code application logic here
+        // Solicitar al usuario que ingrese la enfermedad y el medicamento
+        String enfermedad = JOptionPane.showInputDialog("Ingrese el nombre de la enfermedad:");
+        String medicamento = JOptionPane.showInputDialog("Ingrese el nombre del medicamento para tratar la enfermedad:");
+
+        // Escribir la enfermedad y el medicamento en el archivo de texto
+        escribirEnArchivo(enfermedad, medicamento);
+
+        JOptionPane.showMessageDialog(null, "Información guardada exitosamente en el archivo.");
     }
 }
